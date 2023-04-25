d621789c0af80bb3fb3c6eeb30da313fb2e8db8e
Lana <rnasibullina@gmail.com>
2023-04-25 12:39:33 -0400

    changed AppView

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,18 +1,23 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { TopHeadlinesResponse, SourcesResponse } from '../../types/interfaces';
+
 
 export class AppView {
+    news: News;
+    sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: TopHeadlinesResponse) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: SourcesResponse) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
